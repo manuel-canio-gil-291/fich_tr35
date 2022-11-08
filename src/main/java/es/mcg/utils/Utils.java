f95095fbdcea997414044462d7313af209a4375b
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-08 18:42:44 +0100

    Utilidades

---
+++ src/main/java/es/mcg/utils/Utils.java
@@ -0,0 +1,5 @@
+package es.mcg.utils;
+
+public class Utils {
+    public static final String REGEX = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$";
+}
