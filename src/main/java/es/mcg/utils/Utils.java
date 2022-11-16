d6a0713e12d3e7a5840c3e52b5a444fd98434160
manuel-canio-gil-291 <104891508+manuel-canio-gil-291@users.noreply.github.com>
2022-11-16 13:36:43 +0100

    Utilidades

--- src/main/java/es/mcg/utils/Utils.java
+++ src/main/java/es/mcg/utils/Utils.java
@@ -1,5 +1,11 @@
 package es.mcg.utils;
-
+/**
+ * Utilidades para el ejercicio oculto de Java
+ *
+ * @author Manuel Canio Gil
+ * @version 1.0
+ * @since 2022-11-14
+ */
 public class Utils {
     public static final String REGEX = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$";
 }
